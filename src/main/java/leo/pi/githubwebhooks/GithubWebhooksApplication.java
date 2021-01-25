2fdcc211475a5ad35f314932d0ff6177d8151dd8
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-01-25 21:27:40 +0200

    initial commit

---
+++ src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
@@ -0,0 +1,13 @@
+package leo.pi.githubwebhooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubWebhooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubWebhooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leo/pi/githubwebhooks/GithubWebhooksApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.pi.githubwebhooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubWebhooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
