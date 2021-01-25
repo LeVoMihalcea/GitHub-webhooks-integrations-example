765fc3e0980a335fd20882441727842691f0e761
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-01-25 22:00:24 +0200

    another test

--- src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
+++ src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubWebhooksApplication {
 
-	//change 3
+	//change 4
 	public static void main(String[] args) {
 		SpringApplication.run(GithubWebhooksApplication.class, args);
 	}
