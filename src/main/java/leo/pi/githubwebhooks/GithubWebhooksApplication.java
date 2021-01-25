d0c8f7089540a5e505d74309aa94b4c69180fde8
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-01-25 23:02:25 +0200

    istvan e enervant

--- src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
+++ src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubWebhooksApplication {
 
-	//change 58
+	//change 59
 	public static void main(String[] args) {
 		SpringApplication.run(GithubWebhooksApplication.class, args);
 	}
