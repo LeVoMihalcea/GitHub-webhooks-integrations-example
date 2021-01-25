4b9edee59476969ee3bd5a073dc9adcecb131c3e
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-01-25 22:52:49 +0200

    test

--- src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
+++ src/main/java/leo/pi/githubwebhooks/GithubWebhooksApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubWebhooksApplication {
 
-	//change 55
+	//change 56
 	public static void main(String[] args) {
 		SpringApplication.run(GithubWebhooksApplication.class, args);
 	}
